3e0d740cef5e6fb2f3726215fa60e14e8249d4cf
kor5n <kor5n@pyro.local>
2022-08-11 13:32:21 +0300

    I have made it so you can monitor tickets on tottenham hotspurs fc website(home tickets)

--- TicketBot.py
+++ TicketBot.py
@@ -6,14 +6,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from time import sleep
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
 driver = webdriver.Chrome(options=options, executable_path="/Users/kor5n/Desktop/chromedriver")
-driver.get("https://dif.se/kalender/2020-2029/2022/matcher/herrar/allsvenskan/djurgarden-elfsborg")
-found_tickets = False
+driver.get("https://www.tottenhamhotspur.com/tickets/buy-tickets/home-tickets/")
 
 
 
@@ -31,23 +30,22 @@ class CustomClient(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
     async def on_message(self, message):
-        if message.content.startswith("/HELLO"):
+        if message.content.startswith("/Find Spurs tickets"):
             channel = message.channel
-            await channel.send("I WANT WORK!")    
-    async def monitoring_dif(self, message):
-        while found_tickets != True:
+            await channel.send("YES SIR!")
+            await self.monitoring_spurs(message)    
+    async def monitoring_spurs(self, message):
+        findingTickets = True
+        while findingTickets == True:
+            sleep(10)
             try:
-                waiting = WebDriverWait(driver, 5).until(
-                    EC.element_to_be_clickable((By.Class, "component-cta-button component-cta-button--large"))
-                    )
-            finally:
-                print("We found the button")
+                driver.find_element(By.XPATH, "//div[text() = '25 Oct']")#Type the date when tickets will be out              
+            except:            
                 driver.save_screenshot('screenshot.png')
                 channel = message.channel
-                await channel.send("@everyone!")
                 await channel.send(file=discord.File('screenshot.png'))
-                driver.quit() 
-                found_tickets = True
+                findingTickets = False
+        driver.quit()       
         
             
 
