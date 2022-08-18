484f1f6dbed7826a272ab42f80018220e2376cf7
kor5n <kor5n@pyro.local>
2022-08-18 15:53:04 +0200

    I made that the bot can now monitor the website of my favourite swedish team

--- TicketBot.py
+++ TicketBot.py
@@ -4,15 +4,7 @@ import discord
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-
-driver = webdriver.Chrome(options=options, executable_path="/Users/kor5n/Desktop/chromedriver")
-driver.get("https://www.tottenhamhotspur.com/tickets/buy-tickets/home-tickets/")
 
 
 
@@ -33,21 +25,57 @@ class CustomClient(discord.Client):
         if message.content.startswith("/Find Spurs tickets"):
             channel = message.channel
             await channel.send("YES SIR!")
-            await self.monitoring_spurs(message)    
+            await self.monitoring_spurs(message)
+        if message.content.startswith("/Find DIF tickets"):
+            channel = message.channel
+            await channel.send("YES SIR!")
+            await self.monitoring_dif(message)   
+    async def monitoring_dif(self, message):
+        options = Options()
+        options.headless = True
+        options.add_argument("--window-size=1920,1200")
+
+        driver = webdriver.Chrome(options=options, executable_path="/Users/kor5n/Desktop/chromedriver")
+        driver.get("https://dif.se/kalender/2020-2029/2022/matcher/herrar/allsvenskan/hammarby-djurgarden")
+
+        finding_tickets = True
+        while finding_tickets == True:
+            sleep(10)
+            try:
+                element = driver.find_element(By.XPATH, '//*[@id="ModuleCalendarInformation"]/div/div[1]/div[2]/ul/li/div/div/a')
+                driver.save_screenshot('screenshot.png')
+                channel = message.channel
+                await channel.send("@everyone\nDIF tickets are out!")
+                await channel.send(file=discord.File('screenshot.png'))
+                finding_tickets = False
+                element.click()
+            except:
+                print("we didnt find tickets")
+        driver.quit()
     async def monitoring_spurs(self, message):
+        options = Options()
+        options.headless = True
+        options.add_argument("--window-size=1920,1200")
+
+        driver = webdriver.Chrome(options=options, executable_path="/Users/kor5n/Desktop/chromedriver")
+        driver.get("https://www.tottenhamhotspur.com/tickets/buy-tickets/home-tickets/")
+
         findingTickets = True
         while findingTickets == True:
             sleep(10)
             try:
-                driver.find_element(By.XPATH, "//div[text() = '25 Oct']")#Type the date when tickets will be out              
+                driver.find_element(By.XPATH, "//div[text() = '01 Oct']")#Type the date when tickets will be out
+                print("we didnt find tickets")            
             except:            
-                driver.save_screenshot('screenshot.png')
+                
                 channel = message.channel
-                scroll_down = "window.scrollBy(0,1000);"
-                driver.execute_script(scroll_down)
+                driver.execute_script("window.scrollBy(0,2000);")
+                driver.save_screenshot('screenshot.png')
                 await channel.send(file=discord.File('screenshot.png'))
+                await channel.send("@everyone!\nSpurs tickets are out!")
+                print("we found tickets")
                 findingTickets = False
-        driver.quit()       
+        driver.quit() 
         
             
 
